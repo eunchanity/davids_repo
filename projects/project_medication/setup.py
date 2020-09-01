56629912763070058401a96ac3a74fd3bd192df1
David Weon <davideweon@gmail.com>
2020-09-01 11:41:45 -0400

    project checks

---
+++ projects/project_medication/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='image recognition of common brand names',
+    author='David Weon',
+    license='',
+)
